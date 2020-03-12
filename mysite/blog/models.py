4f1ccd8d4cfffb4923866a6fb292061aa2654e9c
smenaaliaga <smenaaliaga@gmail.com>
2020-03-11 23:46:30 -0300

    Se añade fecha

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+import datetime
 
 class Post(models.Model) :
     title = models.CharField(max_length=200, verbose_name="Título")
     content = models.TextField(verbose_name="Contenido")
+    datePost = datetime.datetime.now()
+    
     class Meta :
         verbose_name = "Entrada"
         verbose_name_plural = "Entradas"
