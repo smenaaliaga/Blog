67dc6df190de114da93f580d264dd32c34dacc29
smenaaliaga <smenaaliaga@gmail.com>
2020-03-17 15:51:20 -0300

    Modificación del modelo, prueba 1

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,11 +1,19 @@
 from django.db import models
 from django.utils import timezone
 
+STATUS = (
+    (0, "Hidden"),
+    (1, "Published")
+)
+
 class Post(models.Model) :
     title = models.CharField(max_length=200, verbose_name="Título")
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_posts')
     content = models.TextField(verbose_name="Contenido")
     published_date = models.DateTimeField(
             default=timezone.now)
+    status = models.IntegerField(choices=STATUS, default=
 
     def publish(self):
         self.published_date = timezone.now()
@@ -17,3 +25,6 @@ class Post(models.Model) :
     
     def __str__(self) :
         return self.title
+
+class Category(models.Model) :
+    category = models.CharField(max_length=200, verbose_name="Categoria")
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, HttpResponse
-from .models import Post 
+from .models import Post    
 import datetime, calendar
 
 def home(request) :
