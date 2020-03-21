2123390f10d2c32bdd913290c1efa74185906ff9
smenaaliaga <smenaaliaga@gmail.com>
2020-03-21 19:05:36 -0300

    Skynet

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -5,10 +5,8 @@ from .models import Post, Category
 admin.site.register(Category)
 # admin.site.register(Post)
 
-# Define the admin class
 class PostAdmin(admin.ModelAdmin) :
     list_display = ('title', 'published_date', 'status')
     list_filter = ('published_date', 'status')
 
-# Register the admin class with the associated model
 admin.site.register(Post, PostAdmin)
